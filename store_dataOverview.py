cfbe6f11adbdfaf82b0ba1a76fb479ae1d32f861
onifade617 <onifade617@gmail.com>
2024-08-09 10:59:28 +0100

    Update store_dataOverview.py

--- store_dataOverview.py
+++ store_dataOverview.py
@@ -8,8 +8,8 @@ Created on Fri Aug  9 09:25:27 2024
 import pandas as pd
 import streamlit as st
 
+df = pd.read_excel("C:/Users/SAIL/Super_store/SuperStoreUS-2015.xlsx")
 
 def main():
     st.title("Data Overview")
-    df = pd.read_excel("C:/Users/SAIL/Super_store/SuperStoreUS-2015.xlsx")
     st.write(df)
