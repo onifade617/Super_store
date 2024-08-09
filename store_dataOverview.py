b66adc687779d508634075f9c31a110cde7f24ad
onifade617 <onifade617@gmail.com>
2024-08-09 11:38:25 +0100

    Update store_dataOverview.py

--- store_dataOverview.py
+++ store_dataOverview.py
@@ -8,7 +8,7 @@ Created on Fri Aug  9 09:25:27 2024
 import pandas as pd
 import streamlit as st
 
-df = pd.read_csv("C:/Users/SAIL/Super_store/SuperStoreUS-2015.csv")9
+df = pd.read_csv("C:/Users/SAIL/Super_store/SuperStoreUS-2015.csv")
 
 def main():
     st.title("Data Overview")
